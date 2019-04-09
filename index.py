fcf2b8b5295d6429f6b0d14d8116bff74b3651b8
jonathandarwin <jonathanjojo38@yahoo.com>
2019-04-09 18:19:18 +0700

    bisa dong

---
+++ index.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from nltk.tokenize import word_tokenize
+
+app = Flask(__name__)
+api = Api(app)
+
+class Note(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("note")
+        args = parser.parse_args()
+
+        print(args["note"])
+
+        listWord = word_tokenize(args["note"])
+        result = {
+            "status" : 200,
+            "message" : "",
+            "result" : listWord
+        }
+        return result,200
+
+api.add_resource(Note, '/note')
+app.run(debug=True)
