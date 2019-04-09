db706567f2e48dd1572a79afa02f3d226386864d
jonathandarwin <jonathanjojo38@yahoo.com>
2019-04-09 18:39:31 +0700

    asdf

--- index.py
+++ index.py
@@ -7,22 +7,23 @@ api = Api(app)
 
 class Note(Resource):
     def post(self):
-        try:
-            parser = reqparse.RequestParser()
-            parser.add_argument("note")
-            args = parser.parse_args()
+        return 'haha', 200
+        # try:
+        #     parser = reqparse.RequestParser()
+        #     parser.add_argument("note")
+        #     args = parser.parse_args()
 
-            print(args["note"])
+        #     print(args["note"])
 
-            listWord = word_tokenize(args["note"])
-            result = {
-                "status" : 200,
-                "message" : "",
-                "result" : listWord
-            }
-            return result,200
-        except:
-            return 'error',400
+        #     listWord = word_tokenize(args["note"])
+        #     result = {
+        #         "status" : 200,
+        #         "message" : "",
+        #         "result" : listWord
+        #     }
+        #     return result,200
+        # except:
+        #     return 'error',400
 
 api.add_resource(Note, '/note')
 app.run(debug=True)
