1d0dabd479d6c0b8dbbc33cfdd8c0defa92f86da
jonathandarwin <jonathanjojo38@yahoo.com>
2019-04-09 18:37:03 +0700

    lagi

--- index.py
+++ index.py
@@ -7,19 +7,22 @@ api = Api(app)
 
 class Note(Resource):
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("note")
-        args = parser.parse_args()
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument("note")
+            args = parser.parse_args()
 
-        print(args["note"])
+            print(args["note"])
 
-        listWord = word_tokenize(args["note"])
-        result = {
-            "status" : 200,
-            "message" : "",
-            "result" : listWord
-        }
-        return result,200
+            listWord = word_tokenize(args["note"])
+            result = {
+                "status" : 200,
+                "message" : "",
+                "result" : listWord
+            }
+            return result,200
+        except:
+            return 'error',400
 
 api.add_resource(Note, '/note')
 app.run(debug=True)
