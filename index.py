508a2f8cdbdf551228400bab0e88ec8c62c31fb8
jonathandarwin <jonathanjojo38@yahoo.com>
2019-06-01 22:22:40 +0700

    add pickle and library

--- index.py
+++ index.py
@@ -1,17 +1,32 @@
 from flask import Flask
-app = Flask(__name__)
-from flask import Flask
 from flask import request
-app = Flask(__name__)
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import json
 import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split, cross_val_score
+import pickle
+
+app = Flask(__name__)
 
 @app.route('/note', methods=['POST'])
-def post():    
-    content = request.get_json()    
-    note = content['note']
-    
-    return note,200
+def post():        
+    note = request.form.get('note') 
+    listWord = word_tokenize(note)  
+    listStopWords = stopwords.words('english')
+    listTemp = []
+    for word in listWord:
+        if(word.lower() not in listStopWords):
+            listTemp.append(word)
+
+    result = {
+        'result' : listTemp
+    }
+    return json.dumps(result)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
